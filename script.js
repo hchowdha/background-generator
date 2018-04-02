e1bdb4bb5ff284d34cec684b24c3300016f8d666
hchowdha <himanshu11435.nith@gmail.com>
2018-04-02 22:26:19 +0530

    added more options

--- script.js
+++ script.js
@@ -3,18 +3,27 @@ var color1 = document.getElementById("color1");
 var color2 = document.getElementById("color2");
 var body =document.getElementById("gradient");
 var direction = document.getElementById("direction");
+var option ="to right";
 var i;
 for( i =0; i< direction.lenght; i++){}
 
 function setColor(){
-  body.style.background= "linear-gradient(to right,"+color1.value+","+color2.value+")";
+  body.style.background= "linear-gradient("+option+","+color1.value+" ,"+color2.value+")";
 
   css.textContent = body.style.background+";";
 }
 
-
+function setdirection(event){
+	
+	option = event.target.value;
+	
+	body.style.background= "linear-gradient("+option+","+color1.value+","+color2.value+" )";
+	css.textContent = body.style.background+";";
+}
 
 
 color1.addEventListener("input",setColor)
 
 color2.addEventListener("input",setColor)
+
+direction.addEventListener("change", setdirection)
